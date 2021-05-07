c59196e9b92205765a17d7fac8426111085ad734
Jan <gobeli@hotmail.ch>
2021-05-07 16:43:43 +0200

    Documentation

--- connect.py
+++ connect.py
@@ -4,33 +4,44 @@ Connection class
 
 """
 from ib_insync import *
-import asyncio
 
 class ib_connect:
     def __init__(self, host='127.0.0.1', port=7497, client_id=1):
+        """
+        Initial parameters of the IB Connection.
+        """
         self.host = host
         self.port = port
         self.client_id = client_id
+
+        self.open_order = False
         self.ticker = None
 
         self.ib = IB()
+
 
     def open_connection(self):
         """ open the connection to IB """
         self.ib.connect(self.host, self.port, clientId=self.client_id)
 
+
     def close_connection(self):
         """ close the connection to IB """
         self.ib.disconnect()
 
-    def start_stream(self, ticker, loopback_function):
-        """
 
-        :param ticker: 
-        :param loopback_function: 
+    def start_stream(self, asset: str, ticker: str, loopback_function: str):
         """
+        This function will connect to IB and start the stream of incoming tick data.
 
-        self.ticker = Forex(ticker)
+        :param asset: forex or stock
+        :param ticker: Ticker of the asset that will be traded.
+        :param loopback_function: Name of the function that will be used to process incoming ticks.
+        """
+        if asset == 'forex':
+            self.ticker = Forex(ticker)
+        elif asset == 'stock':
+            self.ticker = Stock(ticker, 'CHIXCH', 'CHF')
 
         self.ib.reqMktData(self.ticker)
         self.ib.pendingTickersEvent += loopback_function
@@ -38,16 +49,87 @@ class ib_connect:
         while self.ib.waitOnUpdate():
             self.ib.sleep(1)
     
-    def create_marketorder(self, action, amount):
-        marketOrder = MarketOrder(action = action, totalQuantity= amount)
-        trade = self.ib.placeOrder(self.ticker, marketOrder) 
-    
-    def create_limitorder(self, ticker, amount, limit):
-        pass
 
-    def cancel_order(self, id):
-        pass
+    def order_filled(self, trade):
+        """ Routine to report that a trade is executed """
+        self.open_order = False
+        
 
-    def check_order_status(self, id):
-        pass
+    def create_marketorder(self, action: str, amount: int):
+        """
 
+        :param action:
+        :param amount:
+        :return: orderId
+        """
+        if self.balance_check(amount):
+            marketOrder = MarketOrder(action = action, totalQuantity= amount)
+            order = self.ib.placeOrder(self.ticker, marketOrder) 
+            order.filledEvent += self.order_filled
+            return order.order
+        else:
+            print('Not enough money left')
+            return False
+   
+
+    def create_limitorder(self, action: str, amount: int, limit: float):
+        """
+        Create a limit order and save the order id.
+        
+        :param action: Buy or sell.
+        :param amount: amount of shares to trade.
+        :param limit: Limit price at which the trade will be entered.
+        :return: orderID.
+        """
+        if self.balance_check(amount):
+            limitOrder = LimitOrder(action = action, totalQuantity= amount, lmtPrice = limit)
+            order = self.ib.placeOrder(self.ticker, limitOrder)
+            order.filledEvent += self.order_filled
+
+            if order.orderStatus.status == 'Submitted':
+                print('Limit{} order submitted @ {}'.format(action, limit))
+            
+            return order.order
+        else:
+            print('Not enough money left')
+            return False
+
+
+    def cancel_order(self, id_):
+        """
+        cancel orders.
+
+        :param id: orderID that needs to be canceled. 
+        """
+        self.ib.cancelOrder(id_)
+
+
+    def check_order_status(self, id_):
+        status = id_.orderStatus.status
+        while id_.isDone():
+            print('pending')
+            self.ib.sleep(5)
+
+
+    def balance_check(self, amount: int) -> bool:
+        """
+        This function checks if there is enough money left in the account 
+        to execute the current trade.
+
+        :param amount: (int) number of shares that will be traded
+        :return: (bool) True if enough is available, false otherwise.
+        """
+        available = available_balance()
+        if available >= amount: return True
+        else: return False
+
+
+    def available_balance(self) -> float:
+        """
+        Fetch the current available balance on our account.
+
+        :return (int): available balance.
+        """
+        account = ib.accountSummary()
+        available = account[10].value
+        return available
--- test_strategy.py
+++ test_strategy.py
@@ -10,6 +10,7 @@ amount = 5
 ticker = 'EURUSD'
 last_buy = None
 in_long = False
+lookback = 100
 
 data = np.array([0,0,0,0])
 
@@ -18,7 +19,16 @@ ib = ibc.ib_connect()
 ib.open_connection()
 print('Connection established')
 
+def print_statement():
+    pass
+
 def trade_logic():
+    """
+    This is the main function that will perform the trading strategy itself.
+    It will be called every time a new tick arrives to check the trading condition.
+    If the dicision is made to trade, the trades are directly executed using functions
+    defined in the connect.py file.
+    """
     global in_long
     global last_buy
 
@@ -33,14 +43,25 @@ def trade_logic():
         ib.create_marketorder('SELL', amount)
 
 def process_ticks(tick):
-    global data
+    """
+    This function processes incoming ticks and saves them to a local dataframe.
+    The dataframe is then cut so it will only contain the n most recent ticks 
+    necessary for the trading strategy.
+
+    :param tick: tick from IB that needs to be processed. 
+    """
 
     data = np.append(data, (tick.askSize, tick.ask, tick.bid, tick.bidSize))
 
-    if len(data) > 100:
+    if len(data) > lookback:
         data = data[4:]
 
 def new_tick(tickers):
+    """
+    This function is the main loopback function which will be automatically executed 
+    by IB whenever a new tick arrives. The tick is then added to the local dataframe
+    using process_ticks and then the trading logic is executed.
+    """
     for ticker in tickers:
         process_ticks(ticker)
 
